d0f239c5d5f6829e99dabf85e5feb606dd7fe4c5
Hossam Khalifa <hossamkhalifa@Hossams-MacBook-Pro.local>
2015-09-30 08:56:27 +0200

    added test and code for deleting todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,13 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
-
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
 
   	if @todo_item.save
@@ -26,9 +23,7 @@ class TodoItemsController < ApplicationController
 
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
-    flash[:success] = params.inspect
   end
 
 
@@ -41,16 +36,29 @@ class TodoItemsController < ApplicationController
     else
       flash[:error] = "That todo item could not be saved"
       render action: :edit
-
     end
 
   end
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
 
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted"
+    else
+      flash[:error] = "Todo list item could not be deleted"
+    end
+    redirect_to todo_list_todo_items_path
+
+  end
   def url_options
     {todo_list_id:params[:todo_list_id]}.merge(super)
   end
 
   private
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
   def todo_item_params
   	params[:todo_item].permit(:content)
   end
---
+++ spec/features/todo_items/delete_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "editing todo_items" do 
+
+	let!(:todo_list){TodoList.create(title:"My Todo list", description:"My Description")}
+	let!(:todo_item){todo_list.todo_items.create(content:"Milk")}
+	def  visit_todo_list(list) 
+
+		visit "/todo_lists"
+
+		within "#todo_list_#{list.id}" do
+			click_link "List Items"
+		end
+	end
+
+	it"deltes the todo_item  when clicking destroy" do
+		visit_todo_list(todo_list)
+		within"#todo_item_#{todo_list.id}" do
+			click_link "Destroy"
+		end
+		expect(page).to have_content("Todo list item was deleted")
+		expect(page).to_not have_content("Milk")
+		expect(TodoItem.count).to eq(0)
+	end
+end
